dc53b57f7cab48d00368b1221764679f08568b52
Mahdi Rasooli <Mahdirasooli8029@gmail.com>
2024-06-07 15:53:22 +0330

    Version 12

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 console.log('config')
-console.log("Version 12")
+console.log("Version 13")
 console.log('cloned file')
 
--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 console.log('index')
-console.log("Version 12")
+console.log("Version 13")
 console.log('cloned file')
